a3bd2f7e5fe95ed17d851472889cd45c91e20cad
erivanSantos <santos.erivan@academico.ifpb.edu.br>
2025-01-14 23:01:29 -0300

    fiz uma classe no curso para nao apagar o index controller fiz outro classe Mathcontroller porem esta dando erro

---
+++ src/main/java/com/ec/agenda/MathController.java
@@ -0,0 +1,32 @@
+package com.ec.agenda;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    )throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        }
+
+    private Double convertToDouble(String strnumber) {
+        if(strnumber == null) return 0D;
+        // BR 10,25 US 10.25
+        String number = strnumber.replaceAll(",", ".");
+        if(isNumeric(number))return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strnumber){
+        if( strnumber == null )return false;
+        String number = strnumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]* \\.?[0-9]+");
+    }
+}
